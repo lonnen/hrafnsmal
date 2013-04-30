4d8f0cc3cf46d98fbae7aa2e5cba7941f65502f6
lonnen <chris.lonnen@gmail.com>
2013-04-29 18:41:26 -0700

    improved fake stack traces, min 100%+ real talk
    errors now occur, on average, six function calls deep. function names
    are chosen at random from a predefined list of variable length names,
    always starting with some Real Talk.

--- pages/hrafnsmal.js
+++ pages/hrafnsmal.js
@@ -1,6 +1,39 @@
-var a = 1
-try {
-    doSomething();
-} catch(e) {
-    Raven.captureException(e);
+function h() {
+    return h;
 }
+
+var functionNames = [
+    '_',
+    '$',
+    'crimsonTwins',
+    '$someNodeSelection',
+    'realTalk',
+    'reticulatingSplines',
+    'sms',
+    'Zeta'
+];
+
+functionNames.map(function assignProperties(prop) {
+    h[prop] = function() {
+        var rand = function(max) {
+            // returns a random # from 0...max
+            return Math.floor(Math.random() * max);
+        }
+
+        if (rand(6) === rand(6)) {
+            try {
+                var c;
+                c[12];
+            } catch(e) {
+                Raven.captureException(e);
+            }
+            phantom.exit();
+        }
+
+        var choice = functionNames[rand(functionNames.length)];
+        console.log(choice);
+        h[choice]();
+    };
+});
+
+h.realTalk();
